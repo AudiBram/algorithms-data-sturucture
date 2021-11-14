5f0c18541004f3d67d2e4b5bf5a1f3ba924d61f2
Audi Bramastya Oroh <audibram14@gmail.com>
2021-11-15 03:24:18 +0700

    add fizzbuzz and bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] intArray = {55, 12, -12, 32, 1, -44, 7};
+
+        for(int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
+            for(int i = 0; i < lastUnsortedIndex; i++){
+                if(intArray[i] > intArray[i + 1]){
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+        for(int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+
+    }
+    public static void swap(int[] array, int i, int j){
+        if(i == j){
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
