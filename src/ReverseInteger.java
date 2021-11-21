1af855fb4381e26d75be136784dc91a930da7cbd
Audi Bramastya Oroh <audibram14@gmail.com>
2021-11-22 02:57:05 +0700

    add quick sort java

---
+++ src/ReverseInteger.java
@@ -0,0 +1,24 @@
+/**
+ * Given a signed 32-bit integer x, return x with its digits reversed.
+ * 
+ */
+
+public class ReverseInteger {
+    public static int reverse(int x){
+
+        long reversed = 0;
+        int pop;
+
+        while(x != 0){
+            pop = x % 10;
+            x /= 10;
+            reversed = (reversed * 10) + pop;
+
+            if(reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE){
+                return 0;
+            }
+
+        }
+        return (int) reversed;
+    }
+}
