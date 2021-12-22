f16eeb2853ee59eee2b88f938e77d130570ead1c
Audi Bramastya Oroh <audibram14@gmail.com>
2021-12-22 12:11:28 +0700

    update merge
    Signed-off-by: Audi Bramastya Oroh <audibram14@gmail.com>

--- src/Merge.java
+++ src/Merge.java
@@ -1,4 +1,13 @@
 public class Merge{
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        mergeSort(intArray, 0, intArray.length);
+
+        for(int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+    }
     public static void mergeSort(int[] input, int start, int end){
 
         if(end - start < 2){
@@ -7,7 +16,7 @@ public class Merge{
 
         int mid = (start + end) / 2;
         mergeSort(input, start, mid);
-        mergeSort(input, mid + 1, end);
+        mergeSort(input, mid, end);
         merge(input, start, mid, end);
     }
 
