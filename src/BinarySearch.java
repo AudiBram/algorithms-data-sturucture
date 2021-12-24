b3cd5410a27cba026acb0b00f621065c350a5049
Audi Bramastya Oroh <audibram14@gmail.com>
2021-12-24 09:56:53 +0700

    add binary search
    Signed-off-by: Audi Bramastya Oroh <audibram14@gmail.com>

---
+++ src/BinarySearch.java
@@ -0,0 +1,31 @@
+/**
+ * Given an array of integers nums which is sorted in ascending order, and an integer target,
+ * write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+ * <p>
+ * Example:
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ */
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+}
