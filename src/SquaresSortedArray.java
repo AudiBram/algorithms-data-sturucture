528f473da3a4c41c9c82e9b96d84a40a8b0a2a1e
Audi Bramastya Oroh <audibram14@gmail.com>
2021-12-24 10:03:08 +0700

    add square sorted array
    Signed-off-by: Audi Bramastya Oroh <audibram14@gmail.com>

---
+++ src/SquaresSortedArray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/**
+ * Easy
+ *
+ * Diberikan array bilangan bulat yang diurutkan dalam urutan yang tidak menurun,
+ * kembalikan array kuadrat dari setiap angka yang diurutkan dalam urutan yang tidak menurun.
+ *
+ * Example 1:
+ *
+ * Input: nums = [-4,-1,0,3,10]
+ * Output: [0,1,9,16,100]
+ * Explanation: After squaring, the array becomes [16,1,0,9,100].
+ * After sorting, it becomes [0,1,9,16,100].
+ */
+
+public class SquaresSortedArray {
+    public int[] sortedSquares(int[] nums) {
+
+        for(int i = 0; i < nums.length; i++){
+            nums[i] *= nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
