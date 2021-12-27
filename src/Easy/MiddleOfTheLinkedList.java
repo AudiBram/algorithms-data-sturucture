baaf88afedf71511e03dd5b4f7c4e4766bdcc348
Audi Bramastya Oroh <audibram14@gmail.com>
2021-12-27 10:36:36 +0700

    update middle linked list
    Signed-off-by: Audi Bramastya Oroh <audibram14@gmail.com>

--- src/Easy/MiddleOfTheLinkedList.java
+++ src/Easy/MiddleOfTheLinkedList.java
@@ -4,6 +4,12 @@ package Easy;
  * Diberikan head of singly linked list, kembalikan node tengah dari daftar tertaut.
  *
  * Jika ada dua node tengah, kembalikan node tengah kedua.
+ *
+ * Example 1:
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [3,4,5]
+ * Explanation: The middle node of the list is node 3.
  */
 
 public class MiddleOfTheLinkedList {
