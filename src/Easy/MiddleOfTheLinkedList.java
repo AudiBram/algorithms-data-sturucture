17344dbc3e6b306d95451b0c1257176d710b7758
Audi Bramastya Oroh <audibram14@gmail.com>
2021-12-27 10:34:53 +0700

    add middle linked list
    Signed-off-by: Audi Bramastya Oroh <audibram14@gmail.com>

---
+++ src/Easy/MiddleOfTheLinkedList.java
@@ -0,0 +1,20 @@
+package Easy;
+
+/**
+ * Diberikan head of singly linked list, kembalikan node tengah dari daftar tertaut.
+ *
+ * Jika ada dua node tengah, kembalikan node tengah kedua.
+ */
+
+public class MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode a_pointer = head;
+        ListNode b_pointer = head;
+
+        while(b_pointer != null && b_pointer.next != null){
+            a_pointer = a_pointer.next;
+            b_pointer = b_pointer.next.next;
+        }
+        return a_pointer;
+    }
+}
