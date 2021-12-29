47a96e17ec0f0c3de9181a30b0d8a85185652bad
Audi Bramastya Oroh <audibram14@gmail.com>
2021-12-29 14:41:27 +0700

    add flood fill
    Signed-off-by: Audi Bramastya Oroh <audibram14@gmail.com>

---
+++ src/Medium/FloodFill.java
@@ -0,0 +1,23 @@
+package Medium;
+
+public class FloodFill {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+
+        if(image[sr][sc] == newColor) return image;
+        fill(image, sr, sc, image[sr][sc], newColor);
+        return image;
+
+    }
+    public void fill(int[][] image, int i, int j, int color, int newColor){
+        if(i < 0 || j < 0 || i >= image.length || j >= image[0].length || image[i][j] != color){
+            return;
+        }
+
+        image[i][j] = newColor;
+        fill(image, i - 1, j, color, newColor);
+        fill(image, i + i, j, color, newColor);
+        fill(image, i, j - 1, color, newColor);
+        fill(image, i, j + 1, color, newColor);
+
+    }
+}
