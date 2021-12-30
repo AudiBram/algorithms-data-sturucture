f9f576cbe8a86a01b4d57e1923f77f3945be6828
Audi Bramastya Oroh <audibram14@gmail.com>
2021-12-30 16:26:05 +0700

    add populating next right
    Signed-off-by: Audi Bramastya Oroh <audibram14@gmail.com>

---
+++ src/Medium/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,21 @@
+package Medium;
+
+public class PopulatingNextRightPointersInEachNode {
+    public Node connect(Node root) {
+        if (root == null) return root;
+
+        Node leftNode = root;
+        while (leftNode.left != null) {
+            Node head = leftNode;
+            while (head != null) {
+                head.left.next = head.right;
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+                head = head.next;
+            }
+            leftNode = leftNode.left;
+        }
+        return root;
+    }
+}
